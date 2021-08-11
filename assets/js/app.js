7e10136131acab1b793e7e7f811cd2c05c778f3a
Jacob Adamson <jakeladamson@gmail.com>
2021-08-10 19:26:50 -0600

    click event working now, and api is mapped correctly

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,40 @@
 //declare variables
 
-var citySearch = $("#searchBar").val();
-// for loop (or .each jQuery) for today's date i++ <=5????? 
+// for loop (or .each jQuery) for today's date i++ <=5?????
 
 //login key for api needed???
 var keyApi = "&appid=b6145ae695bfe33643e2f7c034c3dab0";
 
 //need to create/append user search list
 
-$("user-history").function(searchHistory) {
-    var searchHistory = $("<li>").addClass("list-group-item").text(citySearch);
-    $(".list").append(searchHistory);
-    console.log(searchHistory);
-};
+// $(".user-history").function(searchHistory)
+function populateHistory() {
+  var searchHistory = $("<li>").addClass("list-group-item").text(citySearch);
+  $(".list").append(searchHistory);
+  console.log(searchHistory);
+}
 
 //create functions for:
 //ajax API
 //create elements, append to section for today's forecast
-$("#todayCast").function(response) {
-    //manip DOM, create elements
-    citySearch = $("<h3>").addClass("todayCard").text(response.name);
-    var userDate = new Date;
-    userDate= $("<h3>").addClass("todayCard").text(date.toLocaleString("en-US"));
-    var conditionIcon = $("<img>").toLocaleString("src", "https://openweathermap.org/img/w/" + weather[0].icon + "png");
-    var todayTemp = $("<h2>").addClass("today-temp").text(Temperature);
+// $("#todayCast").
+function todayCast() {
+  //     //manip DOM, create elements
 
-    //append elements
-    citySearch.append(citySearch, date, Temperature);
+  citySearch = $("<h3>").addClass("todayCard").text(response.name);
+  var userDate = new Date();
+  userDate = $("<h3>").addClass("todayCard").text(date.toLocaleString("en-US"));
+  var conditionIcon = $("<img>").toLocaleString(
+    "src",
+    "https://openweathermap.org/img/w/" + weather[0].icon + "png"
+  );
+  var todayTemp = $("<h2>").addClass("today-temp").text(Temperature);
 
-    console.log(citySearch, date, Temperature);
+  //append elements
+  citySearch.append(citySearch, date, Temperature);
 
-};
+  console.log(citySearch, date, Temperature);
+}
 //ajax API
 //create elements, append to setcion for 5 day forecast
 
@@ -38,12 +42,10 @@ $("#todayCast").function(response) {
 
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
-  $("#h5cast").addClass("show");
+  var citySearch = $("#searchBar").val();
+  // $("#h5cast").addClass("show");
   var urlApi =
-    "api.openweathermap.org/data/2.5/weather?q=" +
-    citySearch +
-    keyApi;
-  citySearch = $("#searchBar").val();
+    "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + keyApi;
 
   $.ajax({
     url: urlApi,
